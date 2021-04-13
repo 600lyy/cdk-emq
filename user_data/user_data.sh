d7310a40f87143e8eb046b9b9a6165b9000395b2
Andy Lu <i_love_shanghai@hotmail.com>
2021-04-13 11:30:39 +0000

    broker emqx

--- user_data/user_data.sh
+++ user_data/user_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://github.com/emqx/emqx/releases/download/v4.3-rc.3/emqx-edge-ubuntu18.04-4.3-rc.3-amd64.deb
-sudo apt install ./emqx-edge-ubuntu18.04-4.3-rc.3-amd64.deb
+wget https://github.com/emqx/emqx/releases/download/v4.3-rc.3/emqx-ubuntu18.04-4.3-rc.3-amd64.deb
+sudo apt install ./emqx-ubuntu18.04-4.3-rc.3-amd64.deb
 sudo emqx start
