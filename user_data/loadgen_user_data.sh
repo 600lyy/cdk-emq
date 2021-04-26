4c30aeb957653dcc60b77d3c89dbdfd03f04889b
William Yang <mscame@gmail.com>
2021-04-26 23:13:00 +0200

    perf(loadgen): turn loadgen sysconfig

--- user_data/loadgen_user_data.sh
+++ user_data/loadgen_user_data.sh
@@ -4,6 +4,10 @@ set -euo pipefail
 sudo bash -c 'echo "session required pam_limits.so" >> /etc/pam.d/common-session'
 sudo bash -c 'echo "*      soft    nofile      500000"  >> /etc/security/limits.conf'
 sudo bash -c 'echo "*      hard    nofile      500000"  >> /etc/security/limits.conf'
+
+sudo sysctl -w net.ipv4.tcp_tw_reuse=1
+sudo sysctl net.ipv4.ip_local_port_range="1025 65534"
+
 sudo apt update
 sudo apt install -y erlang make
 
