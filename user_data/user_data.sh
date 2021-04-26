ffe224d5452bf518dd4efd79d750376be2385a37
William Yang <mscame@gmail.com>
2021-04-27 00:39:38 +0200

    emqx: bump version to 4.3-rc.5

--- user_data/user_data.sh
+++ user_data/user_data.sh
@@ -7,8 +7,8 @@ sudo bash -c 'echo "*      soft    nofile      500000"  >> /etc/security/limits.
 sudo bash -c 'echo "*      hard    nofile      500000"  >> /etc/security/limits.conf'
 
 # Install emqx
-wget https://github.com/emqx/emqx/releases/download/v4.3-rc.4/emqx-ubuntu20.04-4.3-rc.4-amd64.deb
-sudo apt install ./emqx-ubuntu20.04-4.3-rc.4-amd64.deb
+wget https://www.emqx.io/downloads/broker/v4.3-rc.5/emqx-ubuntu20.04-4.3-rc.5-amd64.deb
+sudo apt install ./emqx-ubuntu20.04-4.3-rc.5-amd64.deb
 sudo bash -c 'echo "## ========= cloud user_data start  ===========##" >> /etc/emqx/emqx.conf'
 sudo bash -c 'echo "node.name = emqx@`hostname -f`" >> /etc/emqx/emqx.conf'
 ## MCAST cluster.discovery
