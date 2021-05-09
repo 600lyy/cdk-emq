05884df0da39ca3cc1b9e33eb9c5b9d4ee597e83
William Yang <mscame@gmail.com>
2021-05-09 16:42:31 +0200

    emqx v4.3.0

--- user_data/user_data.sh
+++ user_data/user_data.sh
@@ -7,7 +7,8 @@ sudo bash -c 'echo "*      soft    nofile      500000"  >> /etc/security/limits.
 sudo bash -c 'echo "*      hard    nofile      500000"  >> /etc/security/limits.conf'
 
 # Install emqx
-wget https://www.emqx.io/downloads/broker/v4.3-rc.5/emqx-ubuntu20.04-4.3-rc.5-amd64.deb
+wget https://www.emqx.io/downloads/broker/v4.3.0/emqx-ubuntu20.04-4.3.0-arm64.deb
+
 sudo apt install ./emqx-ubuntu20.04-4.3-rc.5-amd64.deb
 sudo bash -c 'echo "## ========= cloud user_data start  ===========##" >> /etc/emqx/emqx.conf'
 sudo bash -c 'echo "node.name = emqx@`hostname -f`" >> /etc/emqx/emqx.conf'
