0ae2d205bf2924c4f47bf93d800d07357d585dde
William Yang <mscame@gmail.com>
2021-05-13 11:41:01 +0200

    inc emqx fds to 8M.

--- user_data/user_data.sh
+++ user_data/user_data.sh
@@ -3,9 +3,15 @@
 # ulimt fds
 #
 sudo bash -c 'echo "session required pam_limits.so" >> /etc/pam.d/common-session'
-sudo bash -c 'echo "*      soft    nofile      500000"  >> /etc/security/limits.conf'
-sudo bash -c 'echo "*      hard    nofile      500000"  >> /etc/security/limits.conf'
+sudo bash -c 'echo "*      soft    nofile      2000000"  >> /etc/security/limits.conf'
+sudo bash -c 'echo "*      hard    nofile      2000000"  >> /etc/security/limits.conf'
 
+echo "net.ipv4.tcp_tw_reuse=1" >>  /etc/sysctl.d/99-sysctl.conf
+echo "fs.nr_open=8000000" >>  /etc/sysctl.d/99-sysctl.conf
+echo 'net.ipv4.ip_local_port_range="1025 65534"' >>  /etc/sysctl.d/99-sysctl.conf
+
+sysctl -w fs.nr_open=8000000
+sysctl -w net.ipv4.tcp_tw_reuse=1
 # Install emqx
 wget https://www.emqx.io/downloads/broker/v4.3.0/emqx-ubuntu20.04-4.3.0-amd64.deb
 
