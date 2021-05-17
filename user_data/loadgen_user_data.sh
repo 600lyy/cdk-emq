2b90bcb7659f62c80fbbc041b7d34d48e199009f
William Yang <mscame@gmail.com>
2021-05-17 22:04:31 +0200

    refactoring loadgen user data.

--- user_data/loadgen_user_data.sh
+++ user_data/loadgen_user_data.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sudo bash -c 'echo "session required pam_limits.so" >> /etc/pam.d/common-session'
-sudo bash -c 'echo "*      soft    nofile      2000000"  >> /etc/security/limits.conf'
-sudo bash -c 'echo "*      hard    nofile      2000000"  >> /etc/security/limits.conf'
+echo "session required pam_limits.so" >> /etc/pam.d/common-session
+echo "*      soft    nofile      2000000"  >> /etc/security/limits.conf
+echo "*      hard    nofile      2000000"  >> /etc/security/limits.conf
 
 echo "net.ipv4.tcp_tw_reuse=1" >>  /etc/sysctl.d/99-sysctl.conf
 echo "fs.nr_open=8000000" >>  /etc/sysctl.d/99-sysctl.conf
 echo 'net.ipv4.ip_local_port_range="1025 65534"' >>  /etc/sysctl.d/99-sysctl.conf
 
-sudo apt update
-sudo apt install -y erlang make
+apt update
+apt install -y erlang make
 
 cd /root/
 git clone https://github.com/emqx/emqtt_bench
